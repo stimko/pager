d74dd0054c38383f37623775aef42f064a8287b3
venkatesh <ven0226@gmail.com>
2018-09-13 12:02:12 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const Hapi = require('hapi');
+const hapiJwt = require('hapi-auth-jwt2');
+
+// Create a server with a host and port
+const server = Hapi.server({
+    host:'localhost',
+    port: 3000
+});
+
+const people = {
+    1: {
+        id: 1,
+        name: 'Jen Jones'
+    }
+};
+
+const validate = (decoded, request, response) =>  {
+    if (!people[decoded.id]) {
+        return { isValid: false };
+    }
+    else {
+        return { isValid: true };
+    }
+};
+
+async function start() {
+
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+
+    await server.register(hapiJwt);
+
+    server.auth.strategy('jwt', 'jwt',
+        {
+            key: 'topsecretstuff',
+            validate: validate,
+            verifyOptions: { algorithms: ['HS256'] }
+        });
+
+    server.auth.default('jwt');
+
+    server.route({
+        method:'GET',
+        path:'/organizations',
+        config: { auth: 'jwt' },
+        handler:function(request,h) {
+    
+            return'hello world';
+        }
+    });
+
+};
+
+start();
---
+++ resources/organizations.js
@@ -0,0 +1,50 @@
+const mysql = require('mysql2/promise');
+const SQL = require('sql-template-strings')
+
+const getConnection = () => {
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'pager',
+        insecureAuth : true,
+    });
+}
+
+const execute = async (query) => {
+    let connection;
+    try {
+        const connection = await getConnection();
+        const [rows] = await connection.execute(query);
+        return rows;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    } finally {
+        connection && connection.end();
+    }
+
+}
+
+const get = async () => {
+    execute(SQL`SELECT * FROM Organizations`);
+}
+
+const getByName = async (name) => {
+    execute(SQL`SELECT * FROM Organizations WHERE Name = ${name}`);
+}
+
+const getByCode = async (code) => {
+    execute(SQL`SELECT * FROM Organizations WHERE Code = ${code}`);    
+}
+
+const create = async (organization) => {
+    execute(SQL`INSERT INTO Organizations (Name, Description, Url, Type, Code) VALUES ('${organization.name}', '${organization.description}', '${organization.url}', '${organization.type}', '${organization.code});`);
+}
+
+const update = async () => {
+   execute('SELECT * FROM Organizations'); 
+}
+
+const remove = async () => {
+    execute(SQL`DELETE FROM Organizations WHERE CustomerName='Alfreds Futterkiste';`);    
+}
